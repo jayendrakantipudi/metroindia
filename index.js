84a2d06647c2865f3e3b22bc57df9d90919a5d33
Jayendra Kantipudi <40329203+jayendrakantipudi@users.noreply.github.com>
2020-07-23 22:06:42 +0530

    Hosting8

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 
-// const MONGO_PORT = "mongodb+srv://jayendra:jayendra@metro-vfxr0.mongodb.net/test?retryWrites=true&w=majority";
-const MONGO_PORT = 'mongodb://localhost:27017/metro'
+const MONGO_PORT = "mongodb+srv://jayendra:jayendra@metro-vfxr0.mongodb.net/test?retryWrites=true&w=majority";
+// const MONGO_PORT = 'mongodb://localhost:27017/metro'
 
 
 
